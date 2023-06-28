898d286302412e7395ea62099bd2e8f9fe15dd79
Alex Shalimov <alexshalimov2019@gmail.com>
2023-06-28 19:39:07 +0300

    F=Fiz

--- ZalikovkaC#/Program.cs
+++ ZalikovkaC#/Program.cs
@@ -11,11 +11,11 @@ namespace Learn
         static void Main(string[] args)
         {
             int k_1 = 6, k_2 = 5, k_3 = 4, k_4 = 4, k_5 = 4, k_6 = 3, k_7 = 2, k_8 = 2;
-            int VM, F, INF, HIST, TCA, ECO, ENG, ENG_1, FIZ;
+            int VM,  INF, HIST, TCA, ECO, ENG, ENG_1, FIZ;
             Console.WriteLine("Enter the rating for vishmat:");
             VM = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter an assessment in physics:");
-            F = int.Parse(Console.ReadLine());
+            FIZ = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter an assessment in computer science:");
             INF = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter an estimate from history:");
@@ -29,7 +29,7 @@ namespace Learn
             Console.WriteLine("Enter the assessment in physical education:");
             FIZ = int.Parse(Console.ReadLine());
             double rating;
-            rating = 90.0 * (k_1 * VM + k_2 * F + k_3 * INF + k_4 * TCA + k_5 * HIST + k_6 * ECO + k_7 * ENG + k_8 * FIZ)
+            rating = 90.0 * (k_1 * VM + k_2 * FIZ + k_3 * INF + k_4 * TCA + k_5 * HIST + k_6 * ECO + k_7 * ENG + k_8 * FIZ)
                  / ((k_1 + k_2 + k_3 + k_4 + k_5 + k_6 + k_7 + k_8) * 100);
             Console.WriteLine("Your rating score =" + rating);
             Console.ReadLine();
