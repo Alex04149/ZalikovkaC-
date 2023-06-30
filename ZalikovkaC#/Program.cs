8b0eaea9c5c27e31cf9c2454e6d6a7568abc5217
Alex Shalimov <alexshalimov2019@gmail.com>
2023-06-30 20:47:30 +0300

    Appdate code

--- ZalikovkaC#/Program.cs
+++ ZalikovkaC#/Program.cs
@@ -10,28 +10,30 @@ namespace Learn
     {
         static void Main(string[] args)
         {    
-            int higerMath, physics, informatics, history, TCA, ecology, english, PE;
+            int higherMath, physics, informatics, history, TDA, ecology, english, PE;
             Console.WriteLine("Enter the rating for higher math:");
-            higerMath = int.Parse(Console.ReadLine());
+            higherMath = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter an assessment in physics:");
             physics = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter an assessment in computer science:");
             informatics = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter an estimate from history:");
             history = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the TCA score:");
-            TCA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the TDA score:");
+            TDA = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter environmental assessment");
             ecology = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter an assessment in English:");
             english = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter the assessment in physical education:");
             PE = int.Parse(Console.ReadLine());
-            double numeral = (Coeficients.higherMath * higerMath + Coeficients.physics * physics + Coeficients.informatics * informatics
-                + Coeficients.history * history + Coeficients.tca * TCA +Coeficients.english*english
+            double numeral = (Coeficients.higherMath * higherMath + Coeficients.physics * physics + Coeficients.informatics * informatics
+                + Coeficients.history * history + Coeficients.TDA * TDA +Coeficients.english*english
                 + Coeficients.ecology * ecology + Coeficients.PE * PE);   
             double rating;
-            rating = (90.0 * numeral)/ (Coeficients.sum * 100);
+            const double maxAssessmentOfDiscipline = 90;
+            const int maxAssessmentOfRating = 100;
+            rating = (maxAssessmentOfDiscipline * numeral)/ (Coeficients.sum * maxAssessmentOfRating);
             Console.WriteLine("Your rating score =" + rating);
             Console.ReadLine();
         }
--- ZalikovkaC#/Variables.cs
+++ ZalikovkaC#/Variables.cs
@@ -4,17 +4,17 @@ using System;
 using System.ComponentModel;
 using System.Data.SqlTypes;
 
-public class Coeficients
+static public class Coeficients
 {
 	public const int higherMath = 6;
     public const int physics = 5;
     public const int informatics = 4;
-    public const int tca = 4;
+    public const int TDA = 4;
     public const int history = 4;
     public const int ecology = 3;
     public const int english = 2;
     public const int PE = 2;
-    public const int sum = higherMath+physics+informatics+tca+history+ecology+english+PE;
+    public const int sum = higherMath+physics+informatics+TDA+history+ecology+english+PE;
 }
 
 
