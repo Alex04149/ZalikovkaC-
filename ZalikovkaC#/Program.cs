1e1de1f0acd894faffc53e6e1cbad384efb2448e
Alex Shalimov <alexshalimov2019@gmail.com>
2023-07-10 21:39:21 +0300

    Підправив форматування тексту

--- ZalikovkaC#/Program.cs
+++ ZalikovkaC#/Program.cs
@@ -12,7 +12,7 @@ namespace Learn
             double rating;
             const double maxAssessmentOfDiscipline = 90;
             const int maxAssessmentOfRating = 100;
-            int higherMathScore=0, physicsScore=0, informaticsScore= 0, historyScore = 0, TheoryOfDigitalAutomataScore = 0, ecologyScore = 0, englishScore = 0, physicalEducationScore = 0;
+            int higherMathScore = 0, physicsScore = 0, informaticsScore = 0, historyScore = 0, TheoryOfDigitalAutomataScore = 0, ecologyScore = 0, englishScore = 0, physicalEducationScore = 0;
             try
             {
                 Console.WriteLine("Enter the rating for higher math:");
@@ -38,10 +38,10 @@ namespace Learn
                 Console.WriteLine("Your rating score =" + rating);
                 Console.ReadLine();
             }
-            catch {
+            catch
+            {
                 Console.WriteLine("ERROR!PLEASE ENTER RIGHT SCORE!");
             }
-            
         }
     }
 }
--- ZalikovkaC#/Variables.cs
+++ ZalikovkaC#/Variables.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-
-
 public class Coeficients
 {
     public const int higherMath = 6;
