08d09465d2950269600ba78f4ded44116924c951
Alex Shalimov <alexshalimov2019@gmail.com>
2023-07-10 21:25:07 +0300

    Виправлення помилок

--- Variables.cs
+++ Variables.cs
@@ -1,17 +1,5 @@
 ﻿using System;
 
-public class Coeficients
-{
-	public static const int higherMath = 6;
-    public static const int fiziks = 5;
-    public static const int informaticks = 4;
-    public static const int tca = 4;
-    public static const int history = 4;
-    public static const int ecology = 3;
-    public static const int english = 2;
-    public static const int PE = 2;
-
-}
 
 
 public class Class1
---
+++ ZalikovkaC#/Variables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+public class Coeficients
+{
+    public const int higherMath = 6;
+    public const int physics = 5;
+    public const int informatics = 4;
+    public const int TDA= 4;
+    public const int history = 4;
+    public const int ecology = 3;
+    public const int english = 2;
+    public const int PE = 2;
+    public const int sum = higherMath+ physics + informatics+TDA+history+ecology+english+PE;
+
+}
